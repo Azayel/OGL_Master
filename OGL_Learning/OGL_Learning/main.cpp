73b0e627c58f4521f4e7ff7c9b9eca3f045e2ce2
Azayel <Asayel404@proton.me>
2023-10-31 16:41:34 +0100

    help

--- OGL_Learning/OGL_Learning/main.cpp
+++ OGL_Learning/OGL_Learning/main.cpp
@@ -18,16 +18,33 @@ const float height{ 1000 };
 const int cell_dimension = 20;
 const int sq_size = width / cell_dimension;
 
+//Keep track of moved grid coordinates
 float gridx, gridy;
-
-bool vbinit = false;
-bool clicked = false;
+float mousemovx{0}; 
+float mousemovy{ 0 };
+bool drag = false;
 std::vector<float> grid;
 int clickgird[];
 std::vector<float> rectangles;
 std::vector<int> rect_indices;
 unsigned int EBO;
-glm::mat4 projection = glm::ortho(0.0f, width, height, 0.0f, -1.0f, 1.0f);
+//glm::mat4 projection = glm::ortho(0.0f, width, height, 0.0f, -1.0f, 1.0f);
+float rad = 45.0f;
+glm::mat4 projection = glm::perspective(glm::radians(rad),1.0f,0.1f,1000.0f);
+glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 1000.0f);
+glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::mat4 camera = glm::lookAt(cameraPosition, cameraTarget, cameraUp);
+
+
+struct GridSquare {
+    float x;       // Top-left x-coordinate
+    float y;       // Top-left y-coordinate
+    float width;   // Width of the square
+    float height;  // Height of the square
+};
+std::vector<GridSquare> gridSquares;
+
 
 struct RectangleVertex {
     float x, y, z;
@@ -42,16 +59,55 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        
         double xpos, ypos;
-       
         glfwGetCursorPos(window, &xpos, &ypos);
+        
+        //May God Save me for i do not know if i have the strength to go forther. 
+        //The idea is to transform the mouseclick into a ray which shoots of into the grid and determine if an intersection of the ray and a plane exists?????? HELP
+        //this will certainly kill my brain. Too bad.
+
+        //transform the ray from screenspace to world space 2d to 3d. ?
+        //Meaning http://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/
+        //Homogenous coordinates to camera coordinates to world space coordinates
+        //W=1; ? Its a vector isnt it?
+
+        glm::vec4 ray_clip = glm::vec4((float)(2.0 * xpos / width - 1.0), (float)(1.0 - 2.0 * ypos / height), -1.0, 1.0);
+        //get inverse projection because mvp*camera
+
+        glm::mat4 invProjection = glm::inverse(projection);
+        glm::mat4 invView = glm::inverse(camera);
+        glm::vec4 ray_eye = invProjection * ray_clip;
+        ray_eye = glm::vec4(ray_eye.x, ray_eye.y, -1.0, 0.0);
+        glm::vec3 ray_wor = glm::vec3(invView * ray_eye);  
+        ray_wor = glm::normalize(ray_wor); 
+
+        //WELP Now i need to determine an intersection of the square plane but I NEVER SAVED THE GRID VERTICIES. They are permanently stored and drawn from the buffer.............
+        //structs here we go
+
+
         std::cout << xpos << " " << ypos << std::endl;
+
+
+
         double cell_width = width / (double)cell_dimension;
         int column = (int)((xpos- gridx) / cell_width);
         int row = (int)((ypos - gridy) / cell_width);
+        
+        
+        
+        
+        
+        
+        
         std::cout << column << " " << row << " " << cell_width <<  std::endl;
         
-        if (!(column >= 0 && column < cell_dimension && row >= 0 && row < cell_dimension)) return;
+
+        
+
+       
+
+        //if (!(column >= 0 && column < cell_dimension && row >= 0 && row < cell_dimension)) return;
            
         
         //1. Get the 4 Vertex Coordiates
@@ -97,6 +153,12 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, rect_indices.size() * sizeof(unsigned int), rect_indices.data(), GL_STATIC_DRAW);
 
     }
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+        drag = true;
+    }
+    else if(action == GLFW_RELEASE) {
+        drag = false;
+    }
 }
 
 
@@ -106,20 +168,37 @@ void processInput(GLFWwindow* window) {
         glfwSetWindowShouldClose(window, true);
     }
     else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        gridy += 10.0f;
-    }
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
         gridy -= 10.0f;
     }
-    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        gridx -= 10.0f;
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        gridy += 10.0f;
     }
-    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
         gridx += 10.0f;
     }
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        gridx -= 10.0f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS) {
+        rad += 1.0f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
+        rad -= 1.0f;
+    }
+    
     
 
 }
+
+void scroll_callback(GLFWwindow* window , double xoffset, double yoffset){
+    rad -= yoffset*3;
+    if (rad < 1.0f) rad = 1.0f;
+    if (rad > 120.0f) rad = 120.0f;
+    projection = glm::perspective(glm::radians(rad), 1.0f, 0.1f, 1000.0f);
+
+}
+
+
 
 int main( void )
 {
@@ -134,9 +213,9 @@ int main( void )
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glViewport(0, 0, width, height);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     bool vertexBufferInitialized = initializeVertexbuffer();
     if (!vertexBufferInitialized) return -1;
-    vbinit = true;
 
    
     
@@ -145,7 +224,6 @@ int main( void )
     Shader myShader("shader.vert", "shader.frag");
     myShader.use();
     programID = myShader.getID();
-
     while (!glfwWindowShouldClose(window))
     {
         
@@ -213,7 +291,6 @@ bool initializeWindow() {
 
 bool initializeVertexbuffer() {
     
-    
     glGenVertexArrays(2, VertexArrayID);
     //glGenBuffers(1, &elementbufferobject);
     glGenBuffers(2, vertexbuffer);
@@ -250,15 +327,22 @@ bool initializeVertexbuffer() {
 
 
 void updateAnimationLoop(){
-
     // Clear the screen
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    //get Projectionmatrix and pass it into the fragment shader
     int uniformlocation;
     uniformlocation = glGetUniformLocation(programID, "MVP");
     glUniformMatrix4fv(uniformlocation, 1, GL_FALSE, &projection[0][0]);
     
+   
 
+    uniformlocation = glGetUniformLocation(programID, "camera");
+    glUniformMatrix4fv(uniformlocation, 1, GL_FALSE, &camera[0][0]);
+
+    
+    //draw Grid
     uniformlocation = glGetUniformLocation(programID, "acol");
     glUniform3f(uniformlocation, 0.0f, 0.0f, 0.0f);
     uniformlocation = glGetUniformLocation(programID, "addPos");
@@ -266,6 +350,10 @@ void updateAnimationLoop(){
     glBindVertexArray(VertexArrayID[1]);
     glDrawArrays(GL_LINES,0,grid.size());
 
+
+    //Draw Rectangles
+    uniformlocation = glGetUniformLocation(programID, "acol");
+    glUniform3f(uniformlocation, 0.0f, 0.0f, 1.0f);
     glBindVertexArray(VertexArrayID[0]);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glDrawElements(GL_TRIANGLES, rect_indices.size(), GL_UNSIGNED_INT, 0);
--- OGL_Learning/OGL_Learning/shader.vert
+++ OGL_Learning/OGL_Learning/shader.vert
@@ -4,8 +4,10 @@ layout (location = 1) in vec3 aCol;
 out vec3 ourCol;
 uniform vec3 addPos;
 uniform mat4 MVP;	
+uniform mat4 camera;
 void main()
 {
-   gl_Position = MVP * vec4(aPos+addPos, 1.0);
+   mat4 mvp = MVP * camera;
+   gl_Position = mvp * vec4(aPos+addPos, 1.0);
    ourCol = aCol;
 }
